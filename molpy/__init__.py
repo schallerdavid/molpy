5abfa37cb224121a08e4d51de7b01baffe0e4679
David Schaller <schaller.david@yahoo.de>
2020-02-12 09:40:35 +0100

    Initial commit after CMS Cookiecutter creation, version 1.1

---
+++ molpy/__init__.py
@@ -0,0 +1,14 @@
+"""
+molpy
+Molecule playground.
+"""
+
+# Add imports here
+from .molpy import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
