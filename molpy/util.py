c5a17f9a5d7f186baa266f6054ecaeabedae7c69
David Schaller <schaller.david@yahoo.de>
2020-02-12 14:07:50 +0100

    util: added docstring to distance function

--- molpy/util.py
+++ molpy/util.py
@@ -1,3 +1,18 @@
 
 def distance(point1, point2):
+    """
+        Calculate distance between two points.
+
+        Parameters
+        ----------
+        point1 : array_like
+            The first point.
+        point2 : array_like
+            The second point.
+
+        Returns
+        -------
+        distance : float
+            The distance between point1 and point2.
+        """
     return sum([(x1 - x2) ** 2 for x1, x2 in zip(point1, point2)]) ** 0.5
