937c41d5833ce6730226cafffb21a40c2911b982
David Schaller <schaller.david@yahoo.de>
2020-02-12 13:59:20 +0100

    tests: added new test for util.distance

--- molpy/tests/test_util.py
+++ molpy/tests/test_util.py
@@ -1,5 +1,8 @@
 import molpy
+import pytest
 
-def test_distance():
 
-    assert molpy.util.distance([0, 1], [0, 0]) == 1
+@pytest.mark.parametrize("point1, point2, bench", [([0, 1], [0, 0], 1), ([0, 1, 2], [0, 1, -2], 4)])
+def test_distance(point1, point2, bench):
+
+    assert molpy.util.distance(point1, point2) == pytest.approx(bench, abs=1.e-6)
